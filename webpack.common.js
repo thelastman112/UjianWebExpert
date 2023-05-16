5006b16d7dbccda4a23f1c9044359ec477c36600
thelastman112 <rizkymaestro12@gmail.com>
2023-05-16 09:21:15 +0700

    doesnt work offline pwa

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const ImageminPlugin = require('imagemin-webpack-plugin').default
-const WebpackPwaManifest = require('webpack-pwa-manifest')
 const imageminMozjpeg = require('imagemin-mozjpeg')
 const path = require('path')
 
@@ -78,30 +77,6 @@ module.exports = {
 					from: path.resolve('src/public/'),
 					to: path.resolve('dist/')
 
-				}
-			]
-		}),
-		new WebpackPwaManifest({
-			name: 'Pangananku',
-			short_name: 'Panganku',
-			description: 'Looking for a quick and easy way to find the best restaurants in your area? Look no further than Pangananku, the ultimate web app for restaurant search! With our user-friendly interface and powerful search algorithms, we make it easy for you to discover new dining experiences, compare menus and prices, and read reviews from real customers. Whether youre in the mood for Italian, Chinese, or something in between, Pangananku has you covered. Our extensive database of restaurants includes all types of cuisines, from fast food chains to high-end fine dining establishments. And with our convenient filters and sorting options, you can easily narrow down your search to find exactly what youre looking for. But thats not all – Pangananku also offers a range of other helpful features, including maps and directions to each restaurant, user-generated photos and videos, and exclusive deals and discounts. Plus, with our mobile app, you can search for restaurants on the go and never miss out on a great meal again.So why wait? Sign up for Pangananku today and start discovering the best restaurants in your area!',
-			background_color: '#222831',
-			theme_color: '#393E46',
-			display: 'standalone',
-			start_url: '.',
-			icons: [
-				{
-					src: path.resolve('src/public/images/icons/icon.png'),
-					sizes: [96, 128, 192, 256, 384, 512]
-				},
-				{
-					src: path.resolve('src/public/images/icons/icon.png'),
-					size: '1024x1024'
-				},
-				{
-					src: path.resolve('src/public/images/icons/icon.png'),
-					size: '1024x1024',
-					purpose: 'maskable'
 				}
 			]
 		}),
