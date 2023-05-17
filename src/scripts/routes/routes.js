a806c28ec7383ebce4fbece6fd674219c2cd212c
thelastman112 <rizkymaestro12@gmail.com>
2023-05-17 15:54:46 +0700

    add manifest to all page

---
+++ src/scripts/routes/routes.js
@@ -0,0 +1,10 @@
+import NowPlaying from '../views/pages/now-playing'
+import Upcoming from '../views/pages/upcoming'
+import Detail from '../views/pages/detail'
+
+const routes = {
+	'/': NowPlaying,
+	'/now-playing': NowPlaying,
+	'/upcoming': Upcoming,
+	'/detail/:id': Detail
+}
