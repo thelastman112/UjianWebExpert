ac90dc305765ffab09aaec4fd72f8c0b746fd0be
thelastman112 <rizkymaestro12@gmail.com>
2023-05-19 18:28:29 +0700

    #3 done

--- src/scripts/imageInterval.js
+++ src/scripts/imageInterval.js
@@ -1,17 +1,30 @@
 export const imageTransition = () => {
 	const imageChange = document.querySelector('.main-image')
 
-	const heroImage = [
-		'images/heros/hero-image_1.jpg',
-		'images/heros/hero-image_2.jpg',
-		'images/heros/hero-image_3.jpg',
-		'images/heros/hero-image_4.jpg'
+	let heroImage = [
+		'images/heros/hero-image_1-L.jpg',
+		'images/heros/hero-image_2-L.jpg',
+		'images/heros/hero-image_3-L.jpg',
+		'images/heros/hero-image_4-L.jpg'
 	]
 
 	const small = window.matchMedia('(max-width:375px)').matches
 	if(small){
 		heroImage = [
-			
+			'images/heros/hero-image_1-S.jpg',
+			'images/heros/hero-image_2-S.jpg',
+			'images/heros/hero-image_3-S.jpg',
+			'images/heros/hero-image_4-S.jpg'
+		]
+	}
+
+	const medium = window.matchMedia('(max-width:767px)').matches
+	if(medium){
+		heroImage = [
+			'images/heros/hero-image_1-M.jpg',
+			'images/heros/hero-image_2-M.jpg',
+			'images/heros/hero-image_3-M.jpg',
+			'images/heros/hero-image_4-M.jpg'
 		]
 	}
 
